f619068a2dabe294fb1af778db37f734b26ff74f
rhythmcreative <rhythmcreative@MacStudio-de-rhythmcreative.local>
2026-05-28 16:49:04 +0200

    Added native voice intents for stopping and snoozing alarms

--- custom_components/hass-alarmclock/intent.py
+++ custom_components/hass-alarmclock/intent.py
@@ -2,17 +2,24 @@
 from __future__ import annotations
 
 import voluptuous as vol
+import logging
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import intent, config_validation as cv
 
-from .const import DOMAIN
+from .const import DOMAIN, STATE_TRIGGERED
+
+_LOGGER = logging.getLogger(__name__)
 
 INTENT_SET_ALARM = "HassAlarmClockSet"
+INTENT_STOP_ALARM = "HassAlarmClockStop"
+INTENT_SNOOZE_ALARM = "HassAlarmClockSnooze"
 
 async def async_setup_intents(hass: HomeAssistant) -> None:
     """Set up intents."""
     intent.async_register(hass, SetAlarmIntent())
+    intent.async_register(hass, StopAlarmIntent())
+    intent.async_register(hass, SnoozeAlarmIntent())
 
 class SetAlarmIntent(intent.IntentHandler):
     """Handle SetAlarm intents."""
@@ -33,11 +40,8 @@ class SetAlarmIntent(intent.IntentHandler):
         time_str = slots["time"]["value"]
         sound = slots.get("sound", {}).get("value")
 
-        # Try to find the entity_id for this alarm name
-        # We assume the user might say "the bedroom alarm"
         entity_id = f"switch.{name.lower().replace(' ', '_')}_{name.lower().replace(' ', '_')}"
         
-        # Call the service
         service_data = {
             "entity_id": entity_id,
             "time": time_str,
@@ -54,4 +58,85 @@ class SetAlarmIntent(intent.IntentHandler):
 
         response = intent_obj.create_response()
         response.async_set_speech(f"Alarma configurada para las {time_str} en {name}")
+        return response
+
+class StopAlarmIntent(intent.IntentHandler):
+    """Handle StopAlarm intents."""
+
+    intent_type = INTENT_STOP_ALARM
+    slot_schema = {
+        vol.Optional("name"): cv.string,
+    }
+
+    async def async_handle(self, intent_obj: intent.Intent) -> intent.IntentResponse:
+        """Handle the intent."""
+        hass = intent_obj.hass
+        slots = self.async_validate_slots(intent_obj.slots)
+        name = slots.get("name", {}).get("value")
+        
+        target_entity = None
+        
+        if name:
+            target_entity = f"switch.{name.lower().replace(' ', '_')}_{name.lower().replace(' ', '_')}"
+        else:
+            # Look for ANY triggered alarm
+            for entry_id, entry_data in hass.data[DOMAIN].items():
+                device = entry_data["device"]
+                if device.status == STATE_TRIGGERED:
+                    target_entity = f"switch.{device.name.lower().replace(' ', '_')}_{device.name.lower().replace(' ', '_')}"
+                    break
+        
+        if target_entity:
+            await hass.services.async_call(
+                DOMAIN,
+                "stop",
+                {"entity_id": target_entity},
+                blocking=True,
+            )
+            response = intent_obj.create_response()
+            response.async_set_speech("Alarma detenida.")
+            return response
+        
+        response = intent_obj.create_response()
+        response.async_set_speech("No hay ninguna alarma sonando en este momento.")
+        return response
+
+class SnoozeAlarmIntent(intent.IntentHandler):
+    """Handle SnoozeAlarm intents."""
+
+    intent_type = INTENT_SNOOZE_ALARM
+    slot_schema = {
+        vol.Optional("name"): cv.string,
+    }
+
+    async def async_handle(self, intent_obj: intent.Intent) -> intent.IntentResponse:
+        """Handle the intent."""
+        hass = intent_obj.hass
+        slots = self.async_validate_slots(intent_obj.slots)
+        name = slots.get("name", {}).get("value")
+        
+        target_entity = None
+        
+        if name:
+            target_entity = f"switch.{name.lower().replace(' ', '_')}_{name.lower().replace(' ', '_')}"
+        else:
+            for entry_id, entry_data in hass.data[DOMAIN].items():
+                device = entry_data["device"]
+                if device.status == STATE_TRIGGERED:
+                    target_entity = f"switch.{device.name.lower().replace(' ', '_')}_{device.name.lower().replace(' ', '_')}"
+                    break
+        
+        if target_entity:
+            await hass.services.async_call(
+                DOMAIN,
+                "snooze",
+                {"entity_id": target_entity},
+                blocking=True,
+            )
+            response = intent_obj.create_response()
+            response.async_set_speech("Alarma pospuesta.")
+            return response
+        
+        response = intent_obj.create_response()
+        response.async_set_speech("No hay ninguna alarma para posponer.")
         return response
